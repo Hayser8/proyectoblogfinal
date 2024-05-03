9d3542a1451e62725237fddb02a530464f8290e9
Hayser8 <gar22076@uvg.edu.gt>
2024-05-02 22:15:27 -0600

    se crea el componente header

---
+++ blogproyecto/src/components/Header.jsx
@@ -0,0 +1,19 @@
+function Header () {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "10px 20px",
+        height: "70px",
+        backgroundColor: "#214f51",
+        color: "#fff",
+      }}
+    >
+      <h2 style={{ margin: 0 }}>FT | Fútbol total</h2>
+    </div>
+  );
+}
+
+export default Header;
